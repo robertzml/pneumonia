968b169c6bd21f7f3daafc2d5a9ca8229e43fade
robertzml <robertzml@126.com>
2020-02-08 11:31:54 +0800

    init the project.
    Signed-off-by: robertzml <robertzml@126.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,17 @@ module.exports = {
   root: true,
   env: {
     node: true
+  },    
+  extends: ["plugin:vue/essential", "@vue/prettier", "prettier"],
+  plugins: ["prettier"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-unused-vars": "warn",
+    "prettier/prettier": "warn",
+    "vue/no-unused-vars": "warn"    
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
     parser: "babel-eslint"
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   }
 };
